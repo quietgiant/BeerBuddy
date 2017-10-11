97bb97359e7dd4ba73d3b5e3f82fa490904b00de
James Gould <gouljm01@students.ipfw.edu>
2017-10-11 18:32:29 +0000

    You have to respond to modal to close it now. Modal content is pulled from separate page.

--- res/scripts/modalResponse.js
+++ res/scripts/modalResponse.js
@@ -1,6 +1,21 @@
+function openAgeModal(){
+	//do check for cookie
+	//if cookie does not exist run the following:
+	$.ajax({
+        url: 'modal.html',
+        dataType: 'text',
+        success: function(result) {
+          $('body').append(result);
+          $('#ageModal').modal({
+            backdrop: 'static',
+            keyboard: false  // to prevent closing with Esc button (if you want this too)
+          });
+        $('#ageModal').modal('show');
+      }
+    });
+}
 
 function over21(){
 	// add code to cooking this response.
-		$('#memberModal').modal('hide');
-	}
-	//test
+	$('#memberModal').modal('hide');
+}
