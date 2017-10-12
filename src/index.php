de70bf761ca8938afdbbf7f05b743af5f9d8f956
kt <taylkj04@students.ipfw.edu>
2017-10-11 23:02:52 -0400

    added geolocation to obtain current location of user and added filter results to search for nearby liquor stores.

--- src/index.php
+++ src/index.php
@@ -17,54 +17,91 @@
 	<link href="../res/styles/index.css" rel="stylesheet">
 	<link href="../res/styles/navigation_header.css" rel="stylesheet">
 	<!-- api key: AIzaSyAJEWvn1C-4qZbAUdR-QwiBqe-BX1WDMA8  -->
-	<script type="text/javascript" src="js/maps.js"></script>
-	<script type="text/javascript" src="js/geolocation-marker.js"></script>
+	<script type="text/javascript" src="js/map.js"></script>
 	<script type="text/javascript">
-		var map, infoWindow;
+
+		var map; // google map canvas 
+		var locationWindow // window for current location
+		var resultsWindow; // window for filtering results
+
 		function initMap() {
-		  map = new google.maps.Map(document.getElementById('map'), {
-		    center: { lat: 41.0793, lng: -85.1394 },
-		    zoom: 14
-		  });
-		  
-			infoWindow = new google.maps.InfoWindow;
+			// init map in fort wayne
+			var fw = { lat: 41.0793, lng: -85.1394 }
+			map = new google.maps.Map(document.getElementById('map'), {
+          		center: fw,
+          		zoom: 15,
+          		streetViewControl: false,
+          		mapTypeControl: false,
+          		minZoom: 10
+          		//noClear = true // do not clear the map div elements
+        	});
 
-	     	// Try HTML5 geolocation.
+        	// try to determine the current position based on device location
+			var userLocation;
 	        if (navigator.geolocation) {
-	          navigator.geolocation.getCurrentPosition(function(position) {
-	            var pos = {
-	              lat: position.coords.latitude,
-	              lng: position.coords.longitude
-	            };
+	        	navigator.geolocation.getCurrentPosition(function(position) {
+		            userLocation = {
+		            	lat: position.coords.latitude,
+		            	lng: position.coords.longitude
+		            };
+		            locationWindow = new google.maps.InfoWindow({map: map, position: userLocation, content: 'You are here!'});
 
-	            infoWindow.setPosition(pos);
-	            infoWindow.setContent('Location found.');
-	            infoWindow.open(map);
-	            map.setCenter(pos);
-	            map.setZoom(15);
-	          }, function() {
-	            handleLocationError(true, infoWindow, map.getCenter());
-	          });
+					var request = {location: userLocation, radius: 16000, types: ['liquor_store']};
+
+					map.setCenter(userLocation);
+		            map.setZoom(15);
+
+		            // filter for liquor stores
+					resultsWindow = new google.maps.InfoWindow();
+			        var service = new google.maps.places.PlacesService(map);
+			        service.nearbySearch(request, callback);
+	        	}, 
+	        	function() {
+	            	handleLocationError(true, locationWindow, map.getCenter());
+	          	});
 	        } else {
-	          // Browser doesn't support Geolocation
-	          handleLocationError(false, infoWindow, map.getCenter());
+	        	userLocation = fw;
+	        	handleLocationError(false, locationWindow, map.getCenter()); // browser does not support geolocation
 	        }
+
 		}
 
+		function callback(results, status) {
+	        if (status === google.maps.places.PlacesServiceStatus.OK) {
+	          for (var i = 0; i < results.length; i++) {
+	            createMarker(results[i]);
+	          }
+	        }
+	      }
+
+	    function createMarker(place) {
+	    	var placeLoc = place.geometry.location;
+	    	var marker = new google.maps.Marker({
+	        	map: map,
+	        	position: place.geometry.location
+	        });
+
+	        google.maps.event.addListener(marker, 'click', function() {
+	        	resultsWindow.setContent(place.name);
+	        	resultsWindow.open(map, this);
+	        });
+	      }
+
+
 		function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
+	       	infoWindow.setPosition(pos);
+	       	infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+	        infoWindow.open(map);
+	    }
+		
+		google.maps.event.addDomListener(window, 'load', initialize);
 	</script>
 	<!-- 21+ modal dialog js -->
 	<script type="text/javascript" src="js/modalResponse.js"></script>
 
 </head>
 
-<body onload="initMap();//openAgeModal();">
+<body onload="//openAgeModal();">
 
 	<!-- top navigation bar -->
 	<?php $page='index'; include('navigation_header.php'); ?>
@@ -72,11 +109,9 @@
 	<!-- page contents -->
 	<div class="container-fluid">
 	    
-	    <div class="row" style="margin-top:-20px;">
+	    <div class="row" style="margin-top:-10px;">
 
-			<div id="search"> 
-
-			</div>
+			<div id="search"></div>
 
 			<!-- begin map -->
 			<div id="map"></div>
