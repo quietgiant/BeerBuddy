b0c3b66e70d9dd8f1766f07da1a881cd55ac366a
Kurtis Taylor <taylkj04@students.ipfw.edu>
2017-10-20 16:30:08 +0000

    adding dropdown back in to menu bar

--- src/navigation_header.php
+++ src/navigation_header.php
@@ -19,6 +19,18 @@ $nav = "
         <li class=\"".($class = ($page == 'index') ? 'active' : '')."\"><a href=\"/src/index.php\">Explore Deals</a></li>
         <li class=\"".($class = ($page == 'post') ? 'active' : '')."\"><a href=\"/src/post.php\">Post Deal</a></li>
         <li class=\"".($class = ($page == 'search') ? 'active' : '')."\"><a href=\"/src/search.php\">Search Deals</a></li>
+        <li class=\"dropdown ".($class = ($page == 'help') ? 'active' : '')."\">
+          <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">Help<span class=\"caret\"></span></a>
+          <ul class=\"dropdown-menu\">
+            <li class=\"".($class = ($page == 'quickstart') ? 'active' : '')."\"><a href=\"/src/help/quickstart.php\">Quickstart</a></li>
+            <li class=\"".($class = ($page == 'guidelines') ? 'active' : '')."\"><a href=\"/src/help/guidelines.php\">Posting guidelines</a></li>
+            <li class=\"".($class = ($page == 'tips') ? 'active' : '')."\"><a href=\"/src/help/tips.php\">Exploration tips</a></li>
+            <li role=\"separator\" class=\"divider\"></li>
+            <li class=\"dropdown-header\">Account Settings</li>
+            <li><a href=\"#\">View profile</a></li>
+            <li class=\"".($class = ($page == 'recoverPassword') ? 'active' : '')."\"><a href=\"/src/help/recoverPassword.php\">Recover password</a></li>
+          </ul>
+        </li>
       </ul>
       <ul class=\"nav navbar-nav navbar-right\">
         <li class=\"".($class = ($page == 'login') ? 'active' : '')."\"><a href=\"/src/login.php\"><span class=\"glyphicon glyphicon-log-in\"></span>&nbsp;Sign In</a></li>
